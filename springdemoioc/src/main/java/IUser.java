288009fe8f26bf8fc836483a8cff7bc09c5b2bda
shenhx <623724489@qq.com>
2017-06-19 23:54:15 +0800

    1.增加AOP测试，方便理解Spring AOP。

---
+++ springdemoioc/src/main/java/IUser.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Administrator on 2017/6/19 0019.
+ */
+public interface IUser {
+    public void Login(String username, String password);
+}
---
+++ springdemoioc/src/main/java/UserImpl.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2017/6/19 0019.
+ */
+public class UserImpl implements  IUser {
+
+    public void Login(String username, String password) {
+        System.out.println("---------- 程序正在执行  类名： com.laoyangx.Aop.chapter0.UserImpl 方法名:Login ----------------");
+    }
+}
